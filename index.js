b597d01570eec391cdaa6349277bffa6c0baa335
github username <github email address>
2019-10-08 10:07:55 +0100

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-function addingEventListener() {
-    const input = document.getElementById('input');
+
+/*const input = document.getElementById('input');
 input.addEventListener('click', function(event) {
   alert('I was clicked!');
-});
+});*/
+
+
+
+function addingEventListener() {
+  const input = document.querySelector('#input');
+  input.addEventListener('click', function(event) {
+    alert('I was clicked!');
+  });
 }
+
+addingEventListener()  //make sure you call this method
+
+
+  
